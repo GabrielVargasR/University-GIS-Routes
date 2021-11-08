35d5b4432dcd0ba2cab6e78e259c1117975ab3a3
Gabriel Vargas <gvargasr@estudiantec.cr>
2021-11-08 11:32:32 -0600

    [update] query params

--- src/controllers/GeoServerController.js
+++ src/controllers/GeoServerController.js
@@ -4,21 +4,14 @@ class GeoServerController {
     config = {
         method: 'get',
         url: 'http://localhost:8080/geoserver/routes/wfs',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-        proxy: {
-            host: '104.236.174.88',
-            port: 3128
+        params: {
+            request: 'GetFeature',
+            outputformat: 'application/json'
         }
     }
 
     getRoads = () => {
-        const qConfig = {...this.config, params: {
-            request: 'GetFeature',
-            typeNames: 'routes:roads',
-            outputformat: 'application/json'
-        }}
+        const qConfig = {...this.config, params: {...this.config.params, typeNames: 'routes:roads'}}
         return axios(qConfig)
             .then(res=>{
                 return res.data;
@@ -27,11 +20,7 @@ class GeoServerController {
     };
 
     getStops = () => {
-        const qConfig = {...this.config, params: {
-            request: 'GetFeature',
-            typeNames: 'routes:stops',
-            outputformat: 'application/json'
-        }}
+        const qConfig = {...this.config, params: {...this.config.params, typeNames: 'routes:stops'}}
         return axios(qConfig)
             .then(res=>{
                 return res.data;
